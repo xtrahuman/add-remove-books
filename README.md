86fda102117ef0df72eb5e1a6055a9557842234d
unknown <okpara_tochukwu@yahoo.com>
2021-10-19 09:40:51 +0100

    add javascript to add and remove books from localstorage

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# add-remove-books
-add and remove books using javascript objects
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Awesome books
+
+## Description
+> add and remove books using javascript objects
+
+
+
+## Built With
+
+- html
+- css
+- javascript
+
+## Procedure to clone project
+- open your terminal
+- type git clone https://github.com/xtrahuman/add-remove-books.git
+- type cd add-remove-books to change directory to the folder of the project
+- run the project in your local machine
+
+## project link
+click on the website to view project
+
+- link to website: [website link](https://xtrahuman.github.io/capstone-module1/)
+
+## Author 1
+
+👤 **Okpara Tochukwu**
+
+- GitHub: [@xtrahuman](https://github.com/xtrahuman)
+- Twitter: [@xtrahuman](https://twitter.com/xtrahuman)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/tochukwu-okpara-449528197)
+
+## Author 2
+
+👤 **Daniel Shdeed**
+
+- GitHub: [@Danieldotcomcoder](https://github.com/Danieldotcomcoder)
+- Twitter: [@DannyDotcoder](https://twitter.com/DannyDotcoder)
+
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
+
---
+++ index.js
@@ -0,0 +1,77 @@
+
+const addbtn = document.querySelector('.add')
+const bookSection = document.querySelector('.book-section')
+const bookInput = document.querySelector('.add')
+const authorInput = document.getElementById('Author')
+const titleInput = document.getElementById('Title')
+
+
+function getLocalStorage() {
+    return localStorage.getItem('bookar') ? JSON.parse(localStorage.getItem('bookar')) : [];
+  }
+  
+
+function filterbookhtml(allbook,index) {
+let newbook = allbook.splice(index,1)
+   displayBooks(newbook)
+}
+
+const displayBooks = (bookoutput) => {
+  
+   const allbook = bookoutput.map((book) =>
+        `<div class="author-title">
+    <p>${book.author}</p>
+    <p>${book.title}</p>
+    <button type="button"  class="remove" class="btn">remove</button>
+    <hr>
+</div>`  
+    );
+   
+    bookSection.innerHTML=allbook.join('')
+
+    const removebtn = document.querySelectorAll('.remove')
+ 
+    removebtn.forEach((button, index) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+           deleteBookStorage(index)
+           filterbookhtml(bookoutput,index)
+        })
+
+        
+    })
+    function deleteBookStorage(index){
+       
+        filteredbook= bookoutput.filter(indext=> indext !== bookoutput[index])
+        localStorage.setItem('bookar', JSON.stringify(filteredbook));
+      
+    }
+
+    authorInput.value=''
+    titleInput.value=''
+}
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+displayBooks(getLocalStorage())
+})
+
+bookInput.addEventListener('click',(e)=>{
+    e.preventDefault()
+    
+    const bookinfo = {
+        author: authorInput.value,
+        title: titleInput.value
+    }
+   
+    let bookar= getLocalStorage()
+         bookar.push(bookinfo)
+        localStorage.setItem('bookar', JSON.stringify(bookar));
+        
+        displayBooks(getLocalStorage())
+   
+})
+
+
+
+
